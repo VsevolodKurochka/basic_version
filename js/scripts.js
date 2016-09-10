f434169ed9051f3e8db3ea4d8441b0102d7f0099
Vsevolod <dramdoobas123456@gmail.com>
2016-09-11 03:18:54 +0400

    Создано swipe-menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,36 @@
 $(document).ready(function(){
 
+		// VARIABLES
 		var header_menu_name 	= 'header-menu',
 				header_menu 			= $('.' + header_menu_name),
 				button_menu 			= $('.btn-menu'),
-				//button_menu_class = $(button_menu + '-class'),
 				body 							= $(".body");
 
-		function toggler(){
-			body.toggleClass("header-menu-push");
-			button_menu.toggleClass('navbar-toggle-open');
-			header_menu.toggleClass('header-menu-open');
-		}
-		button_menu.click(function(e){
-			toggler();
-			e.stopPropagation();
-		});
+		//MENU
+			//SCRIPTS
+			function toggler(){
+				body.toggleClass("header-menu-push");
+				button_menu.toggleClass('navbar-toggle-open');
+				header_menu.toggleClass('header-menu-open');
+			}
+			button_menu.click(function(e){
+				toggler();
+				e.stopPropagation();
+			});
+			$('.anchor-menu').click(function(){
+				var href = $(this).attr('href');
+				$('body,html').animate({
+					scrollTop: $(href).offset().top
+				},2000);
+				toggler();
+				return false;
+			});
+			body.hammer().on("swiperight", function(){
+				toggler();
+			});
+			body.hammer().on("swipeleft", function(){
+				toggler();
+			});
 		// $(document).click(function(e){
 		// 	if( header_menu.hasClass(header_menu_name + '-open') ) {
 		// 		if ( ! $(e.target).is('.'+header_menu_name + ', .'+header_menu_name+"*") ) {
@@ -22,6 +38,8 @@ $(document).ready(function(){
 		// 		}
 		// 	}
 		// });
+
+		// SCROLL TO BLOCK
 		$('.anchor').click(function(){
 			var href = $(this).attr('href');
 			$('body,html').animate({
@@ -29,15 +47,8 @@ $(document).ready(function(){
 			},2000);
 			return false;
 		});
-		$('.anchor-menu').click(function(){
-			var href = $(this).attr('href');
-			$('body,html').animate({
-				scrollTop: $(href).offset().top
-			},2000);
-			toggler();
-			return false;
-		});
-		$(window).on('hashchange', function(e){
-			history.replaceState ("", document.title, e.originalEvent.oldURL);
+		var attr = $(".clicker").attr("data-open");
+		$(".clicker").click(function(){
+			$(attr).slideToggle();
 		});
 });	
