e52ead04ed4f2ae5b1678545a8f8075db7c3bd65
Vsevolod <dramdoobas123456@gmail.com>
2016-12-11 15:58:22 +0300

    Gulp
    1. Add two folders: app and dist
    2. Add GULP for boost development:
    - SassWatcher
    - AutoPrefixer
    - AutoReloader
    3. Next way - it's studiyng flexbox

---
+++ gulpfile.js
@@ -0,0 +1,55 @@
+var gulp = require('gulp'),
+		sass = require('gulp-sass'),
+		browserSync = require('browser-sync'),
+		del = require('del'),
+		autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('sass', function(){
+	return gulp.src('app/sass/**/*.scss')
+		.pipe(sass({
+			outputStyle: 'expanded'
+		}))
+		.pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true } ))
+		.pipe(gulp.dest('app/css'))
+		.pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('browser-sync', function(){
+	browserSync({
+		server: {
+			baseDir: 'app'
+		},
+		notify: false
+	});
+});
+
+gulp.task('watch', ['browser-sync', 'sass'] ,function(){
+	gulp.watch('app/sass/**/*.scss', ['sass']);
+	gulp.watch('app/*.html', browserSync.reload);
+	gulp.watch('app/*.js', browserSync.reload);
+});
+
+gulp.task('clean', function(){
+	return del.sync('dist');
+});
+
+gulp.task('build', ['clean', 'sass'], function(){
+	var buildCss = gulp.src('app/css/style.css')
+		.pipe(gulp.dest('dist/css'))
+
+	var buildFonts = gulp.src('app/fonts/**/*')
+		.pipe(gulp.dest('dist/fonts'))
+
+	var buildJs = gulp.src('app/js/**/*')
+		.pipe(gulp.dest('dist/js'))
+
+	var buildHtml = gulp.src('app/*.html')
+		.pipe(gulp.dest('dist'))
+});
+
+//If you enter 'gulp', gulp will do 'watch' function
+gulp.task('default', ['watch']);
+
+gulp.task('clear', function(){
+	return cache.clearAll();
+});
